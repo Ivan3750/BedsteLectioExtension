48180dd29fefd3e789207dbbf7c382c5a828a6a9
Johnny JTH <johanohly@gmail.com>
2024-01-09 20:34:34 +0100

    onInstalled background listener

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,6 +1,14 @@
 import { runtime, tabs } from 'webextension-polyfill';
 
-runtime.onInstalled.addListener(() => {
-    console.log('[===== Installed Extension!] =====');
-    tabs.create({ url: 'https://www.google.com' });
+const { version } = runtime.getManifest();
+
+runtime.onInstalled.addListener(({ reason }) => {
+    switch (reason) {
+        case 'install':
+            tabs.create({ url: 'https://johnnyjth.com/bedstelectio/welcome' });
+            break;
+        case 'update':
+            tabs.create({ url: `https://johnnyjth.com/bedstelectio/updated?v=${version}` });
+            break;
+    }
 });
