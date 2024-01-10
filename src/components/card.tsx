8e840e9cee79f58bbeeb5518238c7fcff03549f1
Johnny JTH <johanohly@gmail.com>
2024-01-10 17:24:21 +0100

    Reset CardHeader margin

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -15,7 +15,7 @@ CardHeader.displayName = 'CardHeader';
 
 const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
     ({ className, ...props }, ref) => (
-        <h3 ref={ref} className={cn('font-semibold leading-none tracking-tight', className)} {...props} />
+        <h3 ref={ref} className={cn('font-semibold leading-none tracking-tight !my-0', className)} {...props} />
     ),
 );
 CardTitle.displayName = 'CardTitle';
