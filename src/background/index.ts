98d0e32dc1d9de67960f893c7f144e6d3f22fcea
Johnny JTH <johanohly@gmail.com>
2024-01-10 20:40:41 +0100

    Add version check in onInstalled listener

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,6 +3,9 @@ import { runtime, tabs } from 'webextension-polyfill';
 const { version } = runtime.getManifest();
 
 runtime.onInstalled.addListener(({ reason }) => {
+    if (version === '0.0.0') {
+        return;
+    }
     switch (reason) {
         case 'install':
             tabs.create({ url: 'https://johnnyjth.com/bedstelectio/welcome' });
